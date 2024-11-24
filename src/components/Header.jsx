48fbb2ce04223fd7d20cc3e83c86f14ed401044d
MyangSZ <unoyou@naver.com>
2024-11-24 16:54:40 +0900

    [React] 12일차 과제 init / Tailwind스타일로 바꾸기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="flex justify-between items-center py-10px px-20px bg-[black] h-[60px]">
+      <h2 className="text-[25px] font-[600] pl-[10px]">OZ코딩스쿨</h2>
+      <ul className="flex fustify-center items-center gap-[20px]">
+        <li className="list-none text-[13px] font-normal">로그인</li>
+        <li className="list-none text-[13px] font-normal">회원가입</li>
+        <li className="list-none text-[13px] font-normal">내클래스</li>
       </ul>
     </header>
   );
